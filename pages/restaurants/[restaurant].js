573bea8c3c2e4a47a239d75bb29af1b0049915d0
Tyler <tylerclancy030@gmail.com>
2022-03-25 22:34:49 -0500

    Dynamic routing test

---
+++ pages/restaurants/[restaurant].js
@@ -0,0 +1,7 @@
+import { useRouter } from 'next/router';
+import styles from '../../styles/Home.module.css';
+
+export default function Restaurant() {
+  const router = useRouter();
+  return <h1>Dynamic Restaurant Page {router.query.restaurant}</h1>;
+}
