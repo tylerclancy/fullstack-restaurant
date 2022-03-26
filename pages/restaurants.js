e332b7f14810550d517baac6859fc90720ff6035
Tyler <tylerclancy030@gmail.com>
2022-03-25 22:22:45 -0500

    Create restaurant page and redo home page

--- pages/restaurants.js
+++ pages/restaurants.js
@@ -2,5 +2,5 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return <h1>Restaurant Page</h1>;
 }
